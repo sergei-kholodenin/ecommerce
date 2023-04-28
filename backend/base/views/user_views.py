8717f09913a66fe228781230a96348ecefd6fcaf
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-04-28 15:33:28 +0300

    add login page

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .models import Product
+
 from django.contrib.auth.models import User
-from .serializers import *
+from base.serializers import *
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth.hashers import make_password
@@ -57,18 +58,4 @@ def getUserProfile(request):
 def getUsers(request):
     users = User.objects.all()
     serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProducts(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
-
-
-@api_view(['GET'])
-def getProduct(request, pk):
-    product = Product.objects.get(_id=pk)
-    serializer = ProductSerializer(product, many=False)
     return Response(serializer.data)
