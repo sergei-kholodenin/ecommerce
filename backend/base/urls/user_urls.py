a10ce30415ccb4145decb3ed758d3832a4049d9c
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-05-01 13:21:40 +0300

    create the admin panel

--- backend/base/urls/user_urls.py
+++ backend/base/urls/user_urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 
     path('profile/', views.getUserProfile, name="users-profile"),
     path('profile/update/', views.updateUserProfile, name="user-profile-update"),
-    path('', views.getUserProfile, name="users"),
+    path('', views.getUsers, name="users"),
 
 ]
